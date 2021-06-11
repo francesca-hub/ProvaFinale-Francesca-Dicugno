dff44ed29a0b1425dc837d2bdf87056c55d1954a
francesca-hub <72134762+francesca-hub@users.noreply.github.com>
2021-06-11 12:44:50 +0200

    Add files via upload

---
+++ ProvaFinale-Francesca Dicugno/Prova finale/Prova finale/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Prova_finale
+{
+    class Program
+    {
+        //Risposte domande:
+        //1) c,d
+        //2) b,c
+        //3) d
+
+
+        static void Main(string[] args)
+        {
+            do
+            {
+
+
+                Console.WriteLine("Premi 1 - Registrazione");
+                Console.WriteLine("Premi 0 - Esci");
+
+                string scelta = Console.ReadLine();
+
+                switch (scelta)
+                {
+                    case "1":
+                        GestoreUser.Registrazione();
+
+                        break;
+
+                    case "0":
+                        return;
+                }
+            } while (true);
+        }
+    }
+}
